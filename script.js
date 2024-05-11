628079b26516b6750a15810742d9668143440968
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2024-05-11 13:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i = 0; i < str.length; i++) {
+        let found = false;
+        for (let j = 0; j < str.length; j++) {
+            if (i !== j && str.charAt(i) === str.charAt(j)) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            return str.charAt(i);
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+  
